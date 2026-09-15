97c1a3d9fd4af19ad8fa1c8b2baeaa0c58623bb3
abiyts <64828247+abiyts@users.noreply.github.com>
2026-09-15 16:15:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os
+from telethon import TelegramClient
 
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+
+client = TelegramClient("bot", API_ID, API_HASH)
+
+async def main():
+    print("✅ HabeshaSport bot connected!")
+
+with client:
+    client.loop.run_until_complete(main())
